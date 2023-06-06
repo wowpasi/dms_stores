657be8a4e6ad2bf4255efbaf947a9e3fce4d3d22
Pasindu Lakshan <wijayapasindu475@gmail.com>
2023-06-06 21:14:31 +0530

    a

--- DMS Stores/src/com/orien/dms/main/Splash.java
+++ DMS Stores/src/com/orien/dms/main/Splash.java
@@ -24,7 +24,13 @@ public class Splash extends javax.swing.JFrame {
     public Splash() {
 
         initComponents();
+<<<<<<< Updated upstream
         setIconImage(new ImageIcon("src/com/orien/dms/img/logo.png").getImage());
+=======
+     
+          setIconImage(new ImageIcon("src/com/orien/dms/img/logo.png").getImage());
+
+>>>>>>> Stashed changes
     }
 
     /**
